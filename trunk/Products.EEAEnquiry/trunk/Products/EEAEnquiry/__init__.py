06db50f805b309591ec9956a0c4561f08dc631dc
voineali <voineali@24821446-e7ea-0310-81ac-b189f4507b0b>
2011-03-10 16:16:36 +0000

    Refs #8123   - Fixed DeprecationWarning: CMFCorePermissions

--- trunk/Products.EEAEnquiry/trunk/Products/EEAEnquiry/__init__.py
+++ trunk/Products.EEAEnquiry/trunk/Products/EEAEnquiry/__init__.py
@@ -1,76 +1,27 @@
-# -*- coding: utf-8 -*-
-#
-# File: EEAEnquiry.py
-#
-# Copyright (c) 2006 by []
-# Generator: ArchGenXML Version 1.5.1-svn
-#            http://plone.org/products/archgenxml
-#
-# GNU General Public License (GPL)
-#
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
-# 02110-1301, USA.
-#
+""" Initialize
+"""
+import logging
+logger = logging.getLogger('Products.EEAEnquiry')
 
-__author__ = """unknown <unknown>"""
-__docformat__ = 'plaintext'
+from Products.CMFCore import utils as cmfutils
+from Products.CMFCore import DirectoryView
+from Products.Archetypes import atapi
+from Products.Archetypes import listTypes
+from Products.EEAEnquiry import config
 
-
-# There are three ways to inject custom code here:
-#
-#   - To set global configuration variables, create a file AppConfig.py.
-#       This will be imported in config.py, which in turn is imported in
-#       each generated class and in this file.
-#   - To perform custom initialisation after types have been registered,
-#       use the protected code section at the bottom of initialize().
-#   - To register a customisation policy, create a file CustomizationPolicy.py
-#       with a method register(context) to register the policy.
-
-from zLOG import LOG, INFO, DEBUG
-
-LOG('EEAEnquiry', DEBUG, 'Installing Product')
-
+CustomizationPolicy = None
 try:
     import CustomizationPolicy
-except ImportError:
-    CustomizationPolicy = None
+except ImportError, err:
+    logger.debug(err)
 
-from Globals import package_home
-from Products.CMFCore import utils as cmfutils
-from Products.CMFCore import CMFCorePermissions
-from Products.CMFCore import DirectoryView
-from Products.CMFPlone.utils import ToolInit
-from Products.Archetypes.atapi import *
-from Products.Archetypes import listTypes
-from Products.Archetypes.utils import capitalize
-
-import os, os.path
-
-from Products.EEAEnquiry.config import *
-
-DirectoryView.registerDirectory('skins', product_globals)
+DirectoryView.registerDirectory('skins', config.product_globals)
 DirectoryView.registerDirectory('skins/EEAEnquiry',
-                                    product_globals)
-
-##code-section custom-init-head #fill in your manual code here
-from content import *
-##/code-section custom-init-head
-
+                                    config.product_globals)
 
 def initialize(context):
-    ##code-section custom-init-top #fill in your manual code here
+    """ Zope 2
+    """
     from tools import catalog
 
     cmfutils.ToolInit('Enquiry Tool'
@@ -83,35 +34,31 @@ def initialize(context):
     # imports packages and types for registration
     import content
 
-
     # Initialize portal content
-    all_content_types, all_constructors, all_ftis = process_types(
-        listTypes(PROJECTNAME),
-        PROJECTNAME)
+    all_content_types, all_constructors, all_ftis = atapi.process_types(
+        listTypes(config.PROJECTNAME),
+        config.PROJECTNAME)
 
     cmfutils.ContentInit(
-        PROJECTNAME + ' Content',
+        config.PROJECTNAME + ' Content',
         content_types      = all_content_types,
-        permission         = DEFAULT_ADD_CONTENT_PERMISSION,
+        permission         = config.DEFAULT_ADD_CONTENT_PERMISSION,
         extra_constructors = all_constructors,
         fti                = all_ftis,
         ).initialize(context)
 
     # Give it some extra permissions to control them on a per class limit
-    for i in range(0,len(all_content_types)):
-        klassname=all_content_types[i].__name__
-        if not klassname in ADD_CONTENT_PERMISSIONS:
+    for i in range(0, len(all_content_types)):
+        klassname = all_content_types[i].__name__
+        if not klassname in config.ADD_CONTENT_PERMISSIONS:
             continue
 
-        context.registerClass(meta_type   = all_ftis[i]['meta_type'],
-                              constructors= (all_constructors[i],),
-                              permission  = ADD_CONTENT_PERMISSIONS[klassname])
+        context.registerClass(
+            meta_type   = all_ftis[i]['meta_type'],
+            constructors= (all_constructors[i],),
+            permission  = config.ADD_CONTENT_PERMISSIONS[klassname])
 
     # Apply customization-policy, if theres any
     if CustomizationPolicy and hasattr(CustomizationPolicy, 'register'):
         CustomizationPolicy.register(context)
         print 'Customization policy for EEAEnquiry installed'
-
-    ##code-section custom-init-bottom #fill in your manual code here
-    ##/code-section custom-init-bottom
-
