249ed47cb2bc3b8b38c01e047f2acfa02a9b0aca
tiberich <tiberich@24821446-e7ea-0310-81ac-b189f4507b0b>
2011-11-18 16:08:03 +0000

    Tagged Products.EEAEnquiry 4.1.

---
+++ trunk/Products.EEAEnquiry/tags/4.1/Products/EEAEnquiry/content/interfaces.py
@@ -0,0 +1,9 @@
+""" Interfaces
+"""
+import zope.interface
+
+class IEnquiry(zope.interface.Interface):
+    """ Enquiry marker interface """
+
+class IEnquiryRequestor(zope.interface.Interface):
+    """ EnquiryRequestor marker interface """
