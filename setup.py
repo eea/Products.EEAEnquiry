995bb2a52c14210e25e6328414ff80995b925bec
eduard-fironda <eduard.fironda@eaudeweb.ro>
2017-04-20 13:46:48 +0300

    updated package information
    * Change: updated package information
      [eduard-fironda]

--- setup.py
+++ setup.py
@@ -13,15 +13,24 @@ setup(
  description="EEA Enquiry product",
  long_description=open("README.rst").read() + "\n" +
                   open(os.path.join("docs", "HISTORY.txt")).read(),
- url="https://svn.eionet.europa.eu/projects/"
-     "Zope/browser/trunk/Products.EEAEnquiry",
- classifiers=[
-   "Framework :: Plone",
-   "Programming Language :: Python",
-   ],
- keywords='EEA Enquiry',
- author='Antonio De Marinis (EEA), European Environment Agency',
- author_email='webadmin@eea.europa.eu',
+ # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+          "Framework :: Zope2",
+          "Framework :: Plone",
+          "Framework :: Plone :: 4.0",
+          "Framework :: Plone :: 4.1",
+          "Framework :: Plone :: 4.2",
+          "Framework :: Plone :: 4.3",
+          "Programming Language :: Zope",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 2.7",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+          "License :: OSI Approved :: GNU General Public License (GPL)",
+      ],
+ keywords='EEA Enquiry Add-ons Plone Zope',
+ author='European Environment Agency: IDM2 A-Team',
+ author_email='eea-edw-a-team-alerts@googlegroups.com',
+ url="https://github.com/eea/Products.EEAEnquiry",
  license='GPL',
  packages=find_packages(exclude=['ez_setup']),
  namespace_packages=['Products'],
